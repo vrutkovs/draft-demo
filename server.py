11cbe4f87c994005fad2deba39b2f6cc201f0142
Vadim Rutkovsky <vrutkovs@redhat.com>
2018-02-15 12:49:01 +0100

    project lead is allergic to changes...

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from aiohttp import web
 
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
-    text = "Hello, {}".format(name)
+    text = "Ahoj, {}".format(name)
     return web.Response(text=text)
 
 app = web.Application()
