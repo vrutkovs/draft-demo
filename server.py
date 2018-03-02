15f9cdddf28c54ac0af0edae0045804976ad60e8
Vadim Rutkovsky <vrutkovs@redhat.com>
2018-03-02 22:59:19 +0100

    Herp derp I left the debug in there and forgot to reset errors.

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from aiohttp import web
 
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
-    text = "Ahoj, {}".format(name)
+    text = "Hello, {}".format(name)
     return web.Response(text=text)
 
 app = web.Application()
