46345978323f3f7888ac7c9407ec2f83d7dae57f
Vadim Rutkovsky <vrutkovs@redhat.com>
2018-03-03 22:26:14 +0100

    Update configs for blue-green deployment

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eux
+
+oc new-project blue-green || true
+
+# imagestreams
+oc replace --force -f config/fedora-imagestream.yaml
+oc replace --force -f config/imagestream.yaml
+oc replace --force -f config/buildconfig.yaml
+
+# blue
+oc replace --force -f config/blue-deploymentconfig.yaml
+oc replace --force -f config/blue-service.yaml
+
+# green
+oc replace --force -f config/blue-deploymentconfig.yaml
+oc replace --force -f config/blue-service.yaml
+
+oc replace --force -f config/route.yaml
