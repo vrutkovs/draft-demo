1e6fb26d153587210d7b128f03a73383d11a0aec
Vadim Rutkovsky <vrutkovs@redhat.com>
2018-03-03 15:51:19 +0100

    Add imagestreams and setup script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eux
+
+oc new-project pipelines || true
+
+# imagestreams
+oc replace --force -f config/fedora-imagestream.yaml
+oc replace --force -f config/imagestream.yaml
+
+# untested
+oc replace --force -f config/buildconfig.yaml
+oc replace --force -f config/deploymentconfig.yaml
+oc replace --force -f config/service.yaml
+
+# smoketested
+oc replace --force -f config/deploymentconfig-tested.yaml
+oc replace --force -f config/service-tested.yaml
+oc replace --force -f config/route.yaml
