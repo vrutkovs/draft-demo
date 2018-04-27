e37056215c34acd419607e8cf1a299b4a832fd15
Vadim Rutkovsky <vrutkovs@redhat.com>
2018-04-27 20:20:13 +0200

    Add runme script

--- runme.sh
+++ runme.sh
@@ -1,2 +1,3 @@
-oc project demo-master
+oc new-projecy demo-master || oc project demo-master
 oc new-app --name=hello https://github.com/vrutkovs/openshift-demo
+oc expose svc/hello --hostname hello-demo-master.e8ca.engint.openshiftapps.com
