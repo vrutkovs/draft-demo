d7ebd99b78a28d3d7f6d7a1dd8cefc8f0e197ff7
Vadim Rutkovsky <vrutkovs@redhat.com>
2018-05-04 23:04:15 +0200

    runme.sh: fix typo

--- runme.sh
+++ runme.sh
@@ -1,3 +1,3 @@
-oc new-projecy demo-master || oc project demo-master
+oc new-project demo-master || oc project demo-master
 oc new-app --name=hello https://github.com/vrutkovs/openshift-demo
 oc expose svc/hello --hostname hello-demo-master.e8ca.engint.openshiftapps.com
